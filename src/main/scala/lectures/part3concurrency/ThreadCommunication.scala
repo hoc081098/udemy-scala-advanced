8c0248152ba42e5d94f7cefbc1d937e2d100187b
petrus hoc <hoc081098@gmail.com>
2022-09-10 22:52:55 +0700

    26. JVM Thread Communication Exercises

--- src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -283,5 +283,80 @@ object ThreadCommunication extends App {
     (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
   }
 
-  multiProdCons(nConsumers = 3, nProducers = 6)
+  // multiProdCons(nConsumers = 3, nProducers = 6)
+
+  /*
+    Exercises
+    1) think of an example where notifyAll acts in a different way than notify?
+    2) create a deadlock
+    3) create a livelock
+   */
+
+  // notifyAll
+  def testNotifyAll(): Unit = {
+    val bell = new Object()
+
+    (1 to 10).foreach(i =>
+      new Thread(() => {
+        bell.synchronized {
+          println(s"[thread $i] waiting...")
+          bell.wait()
+          println(s"[thread $i] hooray!")
+        }
+      }).start()
+    )
+
+    new Thread(() => {
+      Thread.sleep(2_000)
+
+      println(s"[announcer] Rock'n roll!")
+      bell.synchronized {
+        bell.notify()
+      }
+    }).start()
+  }
+
+  // testNotifyAll()
+
+  // 2- deadlock
+  case class Friend(name: String) {
+    def bow(other: Friend): Unit = {
+      this.synchronized {
+        println(s"$this: I am bowing to my friend $other")
+        other.rise(this)
+        println(s"$this: My friend $other has risen")
+      }
+    }
+
+    def rise(other: Friend): Unit = {
+      this.synchronized {
+        println(s"this: I am rising to my friend $other")
+      }
+    }
+
+    var side = "right"
+
+    def switchSide(): Unit = {
+      if (side == "right") side = "left"
+      else side = "right"
+    }
+
+    def pass(other: Friend): Unit = {
+      while (this.side == other.side) {
+        println(s"$this: Oh, but please, $other, feel free to pass")
+        switchSide()
+        Thread.sleep(1_000)
+      }
+    }
+  }
+
+  val sam = Friend("Sam")
+  val pierre = Friend("Pierre")
+
+  // new Thread(() => sam.bow(pierre)).start() // sam's lock,     | then pierre's lock
+  // new Thread(() => pierre.bow(sam)).start() // pierre's block, | then sam's block
+
+  // 3 - livelock
+  new Thread(() => sam.pass(pierre)).start()
+  new Thread(() => pierre.pass(sam)).start()
 }
