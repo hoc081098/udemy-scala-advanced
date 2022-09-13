2cc6eadbd47e524c4a292686decd032c7d3cba31
petrus hoc <hoc081098@gmail.com>
2022-09-14 01:35:42 +0700

    28. Futures, Part 2

--- src/main/scala/lectures/part3concurrency/FuturesPromises.scala
+++ src/main/scala/lectures/part3concurrency/FuturesPromises.scala
@@ -2,8 +2,7 @@ package com.hoc081098.udemyscalaadvanced
 package lectures.part3concurrency
 
 import scala.concurrent.*
-import scala.util.Success
-import scala.util.Failure
+import scala.util.{Failure, Random, Success}
 
 // important for futures
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -27,4 +26,83 @@ object FuturesPromises extends App {
   } // SOME thread
 
   Thread.sleep(3_000)
+
+  // mini social network
+  case class Profile(id: String, name: String) {
+    def poke(anotherProfile: Profile): Unit =
+      println(s"${this.name} poking ${anotherProfile.name}")
+  }
+
+  object SocialNetwork {
+    // "database"
+    val names = Map(
+      "fb.id.1-zuck" -> "Mark",
+      "fb.id.2-bill" -> "Bill",
+      "fb.id.0-dummy" -> "Dummy"
+    )
+    val friends = Map(
+      "fb.id.1-zuck" -> "fb.id.2-bill"
+    )
+
+    val random = new Random()
+
+    // API
+    def fetchProfile(id: String): Future[Profile] = Future {
+      // fetching from database
+      Thread.sleep(random.nextInt(300))
+      Profile(id, names(id))
+    }
+
+    def fetchBestFriend(profile: Profile): Future[Profile] = Future {
+      Thread.sleep(random.nextInt(400))
+      val bestFriendId = friends(profile.id)
+      Profile(bestFriendId, names(bestFriendId))
+    }
+  }
+
+  // Client: mark to poke bill
+  val mark = SocialNetwork.fetchProfile("fb.id.1-zuck")
+  //  mark.onComplete {
+  //    case Success(markProfile) => {
+  //      val bill = SocialNetwork.fetchBestFriend(markProfile)
+  //      bill.onComplete {
+  //        case Success(billProfile) => markProfile.poke(billProfile)
+  //        case Failure(exception) => exception.printStackTrace()
+  //      }
+  //    }
+  //    case Failure(exception) => exception.printStackTrace()
+  //  }
+
+  // functional composition of futures
+  // map, flatMap, filter
+  val nameOnTheWall: Future[String] = mark.map(profile => profile.name)
+
+  val marksBestFriend: Future[Profile] = mark.flatMap(profile => SocialNetwork.fetchBestFriend(profile))
+
+  val zucksBestFriendRestricted: Future[Profile] = marksBestFriend.filter(profile => profile.name.startsWith("Z"))
+
+  // for-comprehensions
+  for {
+    mark <- SocialNetwork.fetchProfile("fb.id.1-zuck")
+    bill <- SocialNetwork.fetchBestFriend(mark)
+  } mark.poke(bill)
+
+  Thread.sleep(1_000)
+
+  // fallback
+  val aProfileNoMatterWhat: Future[Profile] = SocialNetwork
+    .fetchProfile("unknown-id")
+    .recover {
+      case e: Throwable => Profile("fb.id.0-dummy", "Forever alone")
+    }
+
+  val aFetchedProfileNoMatterWhat = SocialNetwork
+    .fetchProfile("unknown-id")
+    .recoverWith {
+      case e: Throwable => SocialNetwork.fetchProfile("fb.id.0-dummy")
+    }
+
+  val fallbackResult = SocialNetwork
+    .fetchProfile("unknown-id")
+    .fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
 }
