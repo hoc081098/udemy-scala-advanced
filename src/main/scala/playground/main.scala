f34b7238cafc9c926d00e5d8f8d74df674862547
petrus hoc <hoc081098@gmail.com>
2022-09-14 15:18:45 +0700

    playground: Class Composition with Mixins

--- src/main/scala/playground/main.scala
+++ src/main/scala/playground/main.scala
@@ -1,7 +1,26 @@
 package com.hoc081098.udemyscalaadvanced
 package playground
 
+import scala.collection.immutable.Map
+
+trait Car {
+  def name: String
+
+  def horsePower: Int
+}
+
+trait Marshaller extends Car {
+  def toMap: Map[String, Any] = Map(
+    "name" -> name,
+    "horsePower" -> horsePower
+  )
+}
+
+case class BMW(name: String, horsePower: Int) extends Car
+
 @main
 def main(): Unit = {
+  val bmw = new BMW("F15", 309) with Marshaller
+  println(bmw.toMap)
   println("Hello world!")
 }
